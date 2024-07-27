a90d3d07a29ca9265f8d78c6f78d0e96ae92ca5c
10Dozen <steel.mjr@gmail.com>
2024-07-27 17:34:08 +0300

    test4

--- script/app.py
+++ script/app.py
@@ -2,7 +2,7 @@
 import datetime 
 
 if __name__ == '__main__':
-	with open("page.html", "w", encoding="utf-8") as f:
+	with open("../site/page.html", "w", encoding="utf-8") as f:
 		f.write("<html><body><h1>Hello, world! At " + str(datetime.datetime.now()) + "</h1></body></html>");
 	print("App finished!")
 
