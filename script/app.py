05f6aeb04bbde5d7797c9db26ee2a8115186f7c3
10Dozen <steel.mjr@gmail.com>
2024-07-27 17:19:05 +0300

    test

--- script/app.py
+++ script/app.py
@@ -1,5 +1,7 @@
 
+import datetime 
+
 if __name__ == '__main__':
-	with open("result.txt", "w", encoding="utf-8") as f:
-		f.write("KEK");
+	with open("page.html", "w", encoding="utf-8") as f:
+		f.write("<html><body><h1>Hello, world! At " + str(datetime.datetime.now()) + "</h1></body></html>");
 
