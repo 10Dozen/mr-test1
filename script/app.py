d722bae05246eac1032ab3b48b1d72b073bfded9
10Dozen <steel.mjr@gmail.com>
2024-07-28 20:20:49 +0300

    tst32

--- script/app.py
+++ script/app.py
@@ -1,22 +1,40 @@
 import os
 import datetime 
 
+# Read ISSUE 
+# $.getJSON("https://raw.githubusercontent.com/10Dozen/mr-test1/main/site/test.json",e=>{ console.log(e)})
 if __name__ == '__main__':
-	with open("./site/page.html", "w", encoding="utf-8") as f:
-		f.write("<html><body><h1>Hello, world! At " + str(datetime.datetime.now()) + "</h1></body></html>");
+    mission_name = "CO33 Test Mission"
+    description = "My custom description for the mission"
     
+    tgt_dir_name = 'CO33_Test_Mission'
+    tgt_dir = f"./site/{tgt_dir_name}"
+    
+    os.mkdir(tgt_dir)
+    with open(os.path.join(tgt_dir, "index.html"), "w", encoding="utf-8") as f:
+        f.write("<html>\n")
+        f.write(f"<head><script='data.js'/><script='issue.js'/><title>{mission_name}</title></head>\n")
+        f.write("<body><h1>Hello, world! At " + str(datetime.datetime.now()) + "</h1></body>");
+        f.write("</html>\n")
+	
+    with open(os.path.join(tgt_dir, "data.js"), "w", encoding="utf-8") as f:
+        f.write('var data = {"name": 123}')
+        
+    with open("./envvars"), "w", encoding="utf-8") as f:
+        f.write("TARGET_DIR=" + tgt_dir_name)
+ 
 	with open("./.github/ISSUE_TEMPLATE.md", "w", encoding="utf-8") as f:
 		f.write("---\n")
-		f.write("title: My Custom Mission name\n")
+		f.write(f"title: {mission_name}\n")
 		f.write('labels: ["требуется ревью"]\n')
 		f.write("---\n")
 		f.write("\n")
-		f.write("#### Миссия:\n")
-		f.write("- [Ссылка](https://10dozen.github.io/mr-test1/page.html)\n")
-		f.write("#### Описание \n")
-		f.write("Description\n")
-		f.write("#### Автор:\n")
+		f.write("### Миссия:\n")
+		f.write("- [Ссылка]({{ env.PAGES_BASE_URL }}/{{ env.TARGET_DIR }}/index.html)\n")
+		f.write("### Описание \n")
+		f.write(f"{description}\n")
+		f.write("### Автор:\n")
 		f.write("{{ actor }}\n")
 		f.write("\n")
-
-	print("App finished!")
+        
+    print("App finished!")
