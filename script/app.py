ae56a181c8c5aee13dc040318ffb18916c60338e
10Dozen <steel.mjr@gmail.com>
2024-07-28 19:45:14 +0300

    tst27

--- script/app.py
+++ script/app.py
@@ -5,9 +5,9 @@ if __name__ == '__main__':
 	with open("./site/page.html", "w", encoding="utf-8") as f:
 		f.write("<html><body><h1>Hello, world! At " + str(datetime.datetime.now()) + "</h1></body></html>");
     
-    with open("./envvars", "w", encoding="utf-8") as f:
-        f.write("ISSUE_TITLE=My Mission Title\n")
-        f.write("ISSUE_LINK_TO_PAGE=page.html\n")
-        f.write("ISSUE_DESC=Mission description\n")
+	with open("./envvars", "w", encoding="utf-8") as f:
+		f.write("ISSUE_TITLE=My Mission Title\n")
+		f.write("ISSUE_LINK_TO_PAGE=page.html\n")
+		f.write("ISSUE_DESC=Mission description\n")
 
 	print("App finished!")
